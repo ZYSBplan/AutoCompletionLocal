0903058741d5cb4486a9926ef4827b4f09aa3fb6
ZYSBplan <157969416+ZYSBplan@users.noreply.github.com>
2023-05-06 12:34:26 +0000

    added new features

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -5,4 +5,5 @@ import { runTests } from '@vscode/test-electron';
 async function main() {
 	try {
 		// The folder containing the Extension Manifest package.json
-		// Pa
+		// Passed to `--extensionDevelopmentPath`
+		const extensionDevelopmentP
