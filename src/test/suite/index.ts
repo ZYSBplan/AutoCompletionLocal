dd55db8607ec9ea349882cd756f683fac427d386
ZYSBplan <157969416+ZYSBplan@users.noreply.github.com>
2023-05-18 11:19:05 +0000

    cleanup formatting

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -2,4 +2,7 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run
+export function run(): Promise<void> {
+	// Create the mocha test
+	const mocha = new Mocha({
+	
