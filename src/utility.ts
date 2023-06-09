6832dc607630833c62b09b1fcfe4a10fef4dcc25
ZYSBplan <157969416+ZYSBplan@users.noreply.github.com>
2023-06-09 13:43:31 +0000

    Fixes for travis

--- src/utility.ts
+++ src/utility.ts
@@ -3,4 +3,8 @@ import { workspace } from 'vscode';
 export class CharPairMap {
   private openMapping: { [key: string]: number };
   private closeMapping: { [key: string]: number };
-  private 
+  private charsOpen: string[];
+  private charsClose: string[];
+
+  constructor(
+    char
