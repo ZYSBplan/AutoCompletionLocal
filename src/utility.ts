5181c8645b3e3fe16ac6f39774497e8aa760c239
ZYSBplan <157969416+ZYSBplan@users.noreply.github.com>
2023-06-24 11:36:33 +0000

    version upgrade.

--- src/utility.ts
+++ src/utility.ts
@@ -31,4 +31,6 @@ export class CharPairMap {
     return new CharPairMap(charsOpen, charsClose);
   }
 
-  public isOpen(cha
+  public isOpen(char: string) {
+    return this.openMapping[char] !== undefined;
+ 
